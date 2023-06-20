6678155fb4a4cbad38d4d2c718f79a878cae23b8
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-06-20 10:21:15 +0200

    ✨ Added eslint-plugin-storybook

---
+++ packages/eslint/src/rules/storybook.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from '../utils';
+
+export const storybook = defineConfig({
+  plugins: ['storybook'],
+  overrides: [
+    {
+      files: ['*.stories.@(ts|tsx|js|jsx|mjs|cjs)'],
+      rules: {
+        'import/no-anonymous-default-export': 'off',
+        'storybook/await-interactions': 'error',
+        'storybook/context-in-play-function': 'error',
+        'storybook/default-exports': 'error',
+        'storybook/hierarchy-separator': 'warn',
+        'storybook/no-redundant-story-name': 'warn',
+        'storybook/prefer-pascal-case': 'warn',
+        'storybook/story-exports': 'error',
+        'storybook/use-storybook-expect': 'error',
+        'storybook/use-storybook-testing-library': 'error',
+      },
+    },
+    {
+      files: ['.storybook/main.@(js|cjs|mjs|ts)'],
+      rules: {
+        'storybook/no-uninstalled-addons': 'error',
+      },
+    },
+  ],
+});
