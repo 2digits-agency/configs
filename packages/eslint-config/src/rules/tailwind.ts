ad52039171df90541b94eea34210fea4afce41e7
Niels Mentink <nielsmentink@socialbrothers.nl>
2023-09-17 15:48:30 +0200

    🐛 Fixed tailwind to only export stuff if used in project

--- packages/eslint-config/src/rules/tailwind.ts
+++ packages/eslint-config/src/rules/tailwind.ts
@@ -1,4 +1,5 @@
 import { findUpSync } from 'find-up';
+import { getPackageInfoSync } from 'local-pkg';
 
 import { tailwindFunctions } from '@2digits/constants';
 
@@ -6,15 +7,24 @@ import { defineConfig } from '../utils';
 
 const tailwindConfig = findUpSync(['tailwind.config.ts', 'tailwind.config.js']);
 
-export const tailwind = defineConfig({
-  extends: ['plugin:tailwindcss/recommended'],
-  settings: {
-    tailwindcss: {
-      callees: tailwindFunctions,
-      config: tailwindConfig,
-    },
-  },
-  rules: {
-    'tailwindcss/classnames-order': ['off'],
-  },
-});
+const tailwindVersion = getPackageInfoSync('tailwindcss')?.version;
+
+export const tailwind =
+  tailwindVersion && tailwindConfig
+    ? defineConfig({
+        extends: ['plugin:tailwindcss/recommended'],
+        settings: {
+          tailwindcss: {
+            callees: tailwindFunctions,
+            config: tailwindConfig,
+          },
+        },
+        rules: {
+          'tailwindcss/classnames-order': ['off'],
+        },
+      })
+    : defineConfig({
+        extends: [],
+        settings: undefined,
+        rules: undefined,
+      });
