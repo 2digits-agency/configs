fb7fb3fc7c667981d66cfbbbaf6b741e269213eb
Niels Mentink <nielsmentink@socialbrothers.nl>
2024-06-03 08:26:40 +0200

    🏷️ Fixed types for plugin rules

--- packages/eslint-plugin/src/configs/recommended.ts
+++ packages/eslint-plugin/src/configs/recommended.ts
@@ -1,7 +1,9 @@
+import type { Linter } from 'eslint';
+
 import type { rules } from '../rules';
 
 export const recommended = {
   rules: {
     '@2digits/type-param-names': 'error' as const,
-  } satisfies Record<`@2digits/${keyof typeof rules}`, unknown>,
+  } as Record<`@2digits/${keyof typeof rules}`, Linter.RuleEntry<[]>>,
 };
