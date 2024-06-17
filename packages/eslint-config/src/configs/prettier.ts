c4b1951e88dc2c19d9ac1d316e6cc71e009c3a5f
Niels Mentink <nielsmentink@socialbrothers.nl>
2024-06-17 19:41:29 +0200

    🚨 Fixed jsx-newline

--- packages/eslint-config/src/configs/prettier.ts
+++ packages/eslint-config/src/configs/prettier.ts
@@ -2,15 +2,22 @@ import type { TypedFlatConfigItem } from '../types';
 import { interopDefault } from '../utils';
 
 export async function prettier(): Promise<TypedFlatConfigItem[]> {
-  const prettier = await interopDefault(import('eslint-config-prettier'));
+  const [prettier, react] = await Promise.all([
+    interopDefault(import('eslint-config-prettier')),
+    interopDefault(import('eslint-plugin-react')),
+  ]);
 
   return [
     {
       name: '2digits:prettier',
+      plugins: {
+        react,
+      },
       rules: {
         ...prettier.rules,
 
         'tailwindcss/classnames-order': 'off',
+        'react/jsx-newline': ['error', { prevent: false }],
       },
     },
   ];
