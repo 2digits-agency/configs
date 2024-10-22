77f159d324f69c191f83f620fbdd671cba947429
Niels Mentink <nielsmentink@socialbrothers.nl>
2024-10-22 22:58:05 +0200

    docs(changeset): Updated typescript-eslint

--- packages/eslint-config/src/types.gen.d.ts
+++ packages/eslint-config/src/types.gen.d.ts
@@ -4319,7 +4319,7 @@ Backward pagination arguments
    */
   'ts/no-array-delete'?: Linter.RuleEntry<[]>
   /**
-   * Require `.toString()` to only be called on objects which provide useful information when stringified
+   * Require `.toString()` and `.toLocaleString()` to only be called on objects which provide useful information when stringified
    * @see https://typescript-eslint.io/rules/no-base-to-string
    */
   'ts/no-base-to-string'?: Linter.RuleEntry<TsNoBaseToString>
