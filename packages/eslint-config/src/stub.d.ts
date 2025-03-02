d034606de930e745c70347e8731e7e039dd7b826
Niels Mentink <nielsmentink@socialbrothers.nl>
2025-03-02 23:40:32 +0100

    docs(changeset): Removed unused code

--- packages/eslint-config/src/stub.d.ts
+++ packages/eslint-config/src/stub.d.ts
@@ -1,41 +1,3 @@
-declare module 'eslint-plugin-react-hooks' {
-  import type { ClassicConfig, Linter } from '@typescript-eslint/utils/ts-eslint';
-
-  declare const exprt: {
-    configs: {
-      recommended: ClassicConfig.Config;
-    };
-    rules: NonNullable<Linter.Plugin['rules']>;
-  };
-  export = exprt;
-}
-
-declare module 'eslint-plugin-turbo' {
-  import type { ClassicConfig, Linter } from '@typescript-eslint/utils/ts-eslint';
-
-  declare const exprt: {
-    configs: {
-      recommended: ClassicConfig.Config;
-    };
-    rules: NonNullable<Linter.Plugin['rules']>;
-  };
-  export = exprt;
-}
-
-declare module 'eslint-plugin-react' {
-  import type { ClassicConfig, Linter } from '@typescript-eslint/utils/ts-eslint';
-
-  declare const exprt: {
-    configs: {
-      recommended: ClassicConfig.Config;
-      all: ClassicConfig.Config;
-      'jsx-runtime': ClassicConfig.Config;
-    };
-    rules: NonNullable<Linter.Plugin['rules']>;
-  };
-  export = exprt;
-}
-
 declare module '@next/eslint-plugin-next' {
   import type { ClassicConfig, Linter } from '@typescript-eslint/utils/ts-eslint';
 
@@ -77,21 +39,6 @@ declare module 'eslint-plugin-tailwindcss' {
     configs: {
       recommended: ClassicConfig.Config;
       'flat/recommended': FlatConfig.ConfigArray;
-    };
-    rules: NonNullable<Linter.Plugin['rules']>;
-  };
-  export = exprt;
-}
-
-declare module 'eslint-plugin-storybook' {
-  import type { ClassicConfig, Linter } from '@typescript-eslint/utils/ts-eslint';
-
-  declare const exprt: {
-    configs: {
-      recommended: ClassicConfig.Config;
-      csf: ClassicConfig.Config;
-      'csf-strict': ClassicConfig.Config;
-      'addon-interactions': ClassicConfig.Config;
     };
     rules: NonNullable<Linter.Plugin['rules']>;
   };
