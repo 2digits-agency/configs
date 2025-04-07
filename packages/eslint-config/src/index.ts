308bb676146788767899e08fe98f3298c97cf632
Niels Mentink <nielsmentink@socialbrothers.nl>
2025-04-07 22:58:12 +0200

    ⚰️ Removed unused exports from index

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -1,5 +1,3 @@
-export * from './configs';
 export type * from './types';
-export * from './factory';
 
-export { twoDigits as default } from './factory';
+export { twoDigits as default, twoDigits } from './factory';
