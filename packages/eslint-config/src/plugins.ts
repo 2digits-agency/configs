76cb79f9661faa953ea889be21f20dec1937ec98
Niels Mentink <nielsmentink@socialbrothers.nl>
2025-04-07 22:56:17 +0200

    ♻️ Removed plugins.ts

--- packages/eslint-config/src/plugins.ts
+++
@@ -1,3 +0,0 @@
-export { default as pluginUnicorn } from 'eslint-plugin-unicorn';
-export { default as pluginNode } from 'eslint-plugin-n';
-export { default as pluginSonar } from 'eslint-plugin-sonarjs';
